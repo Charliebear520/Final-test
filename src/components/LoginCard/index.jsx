1a087a701129239187537b3b7dce35fc49fa6bcb
Charliebear <50406s97116@gmail.com>
2023-05-29 14:33:01 +0800

    finish login modal function

--- src/components/LoginCard/index.jsx
+++ src/components/LoginCard/index.jsx
@@ -1,18 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { Form, Input, Button, Checkbox } from "antd";
+import { Form, Input, Button, Checkbox, Modal } from "antd";
 import { WarningOutlined, MailOutlined, LockOutlined } from "@ant-design/icons";
 
 import { useSignInWithEmailPassword } from "../../react-query";
 
 import styles from "./logincard.module.css";
 
-const LoginCard = ({ redirect }) => {
+const LoginCard = ({ redirect, isOpen, toggleModal }) => {
   const { mutate, error, isLoading, isError, isSuccess, data } =
     useSignInWithEmailPassword();
   const [isRemember, setIsRemember] = useState(false);
   const [form] = Form.useForm();
   const navigate = useNavigate();
+  const handleCancel = () => toggleModal(!isOpen);
 
   const onFinish = (values) => {
     console.log("Received values of form: ", values);
@@ -26,95 +27,103 @@ const LoginCard = ({ redirect }) => {
   }, [isSuccess, redirect]);
 
   return (
-    <Form
-      name="normal_login"
-      className={styles.loginForm}
-      form={form}
-      initialValues={{
-        isRemember: true,
-      }}
-      onFinish={onFinish}
-      // onFihishFailed={onFinishFailed}
+    <Modal
+      title="Member Login"
+      open={isOpen}
+      onCancel={handleCancel}
+      footer={null}
     >
-      <Form.Item
-        name="email"
-        rules={[
-          {
-            type: "email",
-            message: "The input is not valid E-mail!",
-          },
-          {
-            required: true,
-            message: "Please input your E-mail!",
-          },
-        ]}
-        hasFeedback
+      <Form
+        name="normal_login"
+        className={styles.loginForm}
+        form={form}
+        initialValues={{
+          isRemember: true,
+        }}
+        onFinish={onFinish}
+        // onFihishFailed={onFinishFailed}
       >
-        <Input prefix={<MailOutlined />} placeholder="E-Mail" />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: "Please input your Password!",
-          },
-        ]}
-        hasFeedback
-      >
-        <Input.Password
-          prefix={<LockOutlined />}
-          type="password"
-          placeholder="Password"
-        />
-      </Form.Item>
-      <Form.Item>
-        <Link className={styles.loginForm__forgot} to={"/"}>
-          Forgot password
-        </Link>
-        <Form.Item name="remember" valuePropName="checked" noStyle>
-          <Checkbox
-            onChange={() => setIsRemember(!isRemember)}
-            checked={isRemember}
-          >
-            Remember me
-          </Checkbox>
+        <Form.Item
+          name="email"
+          rules={[
+            {
+              type: "email",
+              message: "The input is not valid E-mail!",
+            },
+            {
+              required: true,
+              message: "Please input your E-mail!",
+            },
+          ]}
+          hasFeedback
+        >
+          <Input prefix={<MailOutlined />} placeholder="E-Mail" />
         </Form.Item>
-      </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Password!",
+            },
+          ]}
+          hasFeedback
+        >
+          <Input.Password
+            prefix={<LockOutlined />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Link className={styles.loginForm__forgot} to={"/"}>
+            Forgot password
+          </Link>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox
+              onChange={() => setIsRemember(!isRemember)}
+              checked={isRemember}
+            >
+              Remember me
+            </Checkbox>
+          </Form.Item>
+        </Form.Item>
 
-      <Form.Item>
-        {isLoading ? (
-          <Button
-            type="primary"
-            htmlType="submit"
-            className={styles.loginForm__button}
-            loading
-          >
-            Log in
-          </Button>
-        ) : (
-          <Button
-            type="primary"
-            htmlType="submit"
-            className={styles.loginForm__button}
-          >
-            Log in
-          </Button>
-        )}
-        Or <Link to={`/auth/register?redirect=${redirect}`}>register now!</Link>
-        {!isError ? (
-          <></>
-        ) : (
-          <div className={styles.loginForm__errorWrap}>
-            <h3 className={styles.loginForm__errorTitle}>
-              <WarningOutlined />
-              {"  "}There was a problem
-            </h3>
-            <p className={styles.loginForm__errorMessage}>{error.message}</p>
-          </div>
-        )}
-      </Form.Item>
-    </Form>
+        <Form.Item>
+          {isLoading ? (
+            <Button
+              type="primary"
+              htmlType="submit"
+              className={styles.loginForm__button}
+              loading
+            >
+              Log in
+            </Button>
+          ) : (
+            <Button
+              type="primary"
+              htmlType="submit"
+              className={styles.loginForm__button}
+            >
+              Log in
+            </Button>
+          )}
+          Or{" "}
+          <Link to={`/auth/register?redirect=${redirect}`}>register now!</Link>
+          {!isError ? (
+            <></>
+          ) : (
+            <div className={styles.loginForm__errorWrap}>
+              <h3 className={styles.loginForm__errorTitle}>
+                <WarningOutlined />
+                {"  "}There was a problem
+              </h3>
+              <p className={styles.loginForm__errorMessage}>{error.message}</p>
+            </div>
+          )}
+        </Form.Item>
+      </Form>
+    </Modal>
   );
 };
 
--- src/components/UserInfo/index.jsx
+++ src/components/UserInfo/index.jsx
@@ -1,31 +1,38 @@
 import { UserOutlined, UserSwitchOutlined } from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
 import { useUserInfo } from "../../react-query";
+import { useState } from "react";
 import styles from "./userinfo.module.css";
+import BasketModal from "../LoginCard";
 
 export default function UserInfo(props) {
   const { data: userInfo } = useUserInfo();
   const navigate = useNavigate();
-
+  const [isOpen, setIsOpen] = useState(false);
   const goToProfile = () => {
     if (userInfo?.name) navigate("/auth/profile");
     else navigate("/auth/login?redirect=/auth/profile");
   };
+  const toggleOpen = () => setIsOpen(!isOpen);
 
   return (
-    <div
-      onClick={goToProfile}
-      style={{ ...props.style }}
-      className={styles.userInfo}
-    >
-      {userInfo ? (
-        <UserOutlined className={styles.userInfoOutlined} />
-      ) : (
-        <UserSwitchOutlined className={styles.userInfoOutlined} />
-      )}
-      <p className={styles.userInfoText}>
-        {!!userInfo?.name ? `${userInfo.name}'s` : `請登入`}
-      </p>
-    </div>
+    <>
+      <div
+        // onClick={goToProfile}
+        onClick={toggleOpen}
+        style={{ ...props.style }}
+        className={styles.userInfo}
+      >
+        {userInfo ? (
+          <UserOutlined className={styles.userInfoOutlined} />
+        ) : (
+          <UserSwitchOutlined className={styles.userInfoOutlined} />
+        )}
+        <p className={styles.userInfoText}>
+          {!!userInfo?.name ? `${userInfo.name}'s` : `請登入`}
+        </p>
+      </div>
+      <BasketModal isOpen={isOpen} toggleModal={toggleOpen} />
+    </>
   );
 }
